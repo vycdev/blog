669b58afa1f8bae1c49be2e163ba760504390c8b
Victor Sandu <vyctor661@gmail.com>
2025-03-04 14:33:42 +0200

    update stuff

--- quartz.config.ts
+++ quartz.config.ts
@@ -17,7 +17,7 @@ const config: QuartzConfig = {
     baseUrl: "blog.vycdev.com",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
-    generateSocialImages: true,
+    generateSocialImages: false,
     theme: {
       fontOrigin: "googleFonts",
       cdnCaching: true,
